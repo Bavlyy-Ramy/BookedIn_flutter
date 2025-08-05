95a0904fcb63113fcada7787ead3d6e0f81d2236
Bavlyy-Ramy <bavly.ramy54@gamil.com>
2025-08-05 17:10:25 +0300

    Initial Setup

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:bookedin_app/features/auth/presentation/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(textTheme: GoogleFonts.abhayaLibreTextTheme()),
+      home: HomePage(),
+    );
+  }
+}
